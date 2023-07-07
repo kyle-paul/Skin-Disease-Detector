a08ab63fefad8c032f2ebd8514ed2ebbcfab9d18
Paul Delta <117391498+DeltaPaul4@users.noreply.github.com>
2023-07-07 10:22:33 +0700

    add uploader

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template, request
-import urllib.request
+from flask import Flask, render_template, request, redirect, url_for
+import os
+# import urllib.request
 from werkzeug.utils import secure_filename
 import numpy as np
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+app.secret_key = "encoder@file"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 10 # 10 MB limit
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-app.secret_key = "encoder@file"
-app.config['UPLOAD_FOLDER'] = 'static/uploads/'
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 10
-
-@app.route("/experience", method=['GET', 'POS'])
+@app.route("/experience")
 def experience():
     return render_template("experience.html")
 
@@ -25,6 +27,25 @@ def github():
 @app.route("/help")
 def help():
     return render_template("help.html")
+
+# A function to check if the file extension is allowed
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# A route to handle the file upload
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    if 'file' not in request.files:
+        return "No file part"
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file"
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('experience'))
+    else:
+        return "File not allowed"
 
 if __name__ == "__main__":
     app.run(debug=True)
